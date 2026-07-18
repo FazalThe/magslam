220b7b9f8dcf6a6ec1a9074e2ee930df3d224edb
FazalThe <fmuhammed344@gmail.com>
2026-07-18 11:38:01 +0530

    made collison shape to sector

--- area_2d.gd
+++ area_2d.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
-const force = 800
+const rforce = 800
+const aforce = 700
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("Left"):
-		print("leftpress")
 		for body: RigidBody2D in get_overlapping_bodies():
 			var dir = global_position.direction_to(body.global_position)
-			body.apply_central_force(force*dir)
+			body.apply_central_force(aforce*dir)
+	
+	if Input.is_action_pressed("Right"):
+		for body: RigidBody2D in get_overlapping_bodies():
+			var dir = -global_position.direction_to(body.global_position)
+			body.apply_central_force(rforce*dir)
